acdb84cab544785d77e0279ca54b9351899350d2
Mian Haseeb <88288001+MianHaseeb787@users.noreply.github.com>
2024-12-21 16:37:08 +0500

    Update items.py

--- basic_scrapy_spider/items.py
+++ basic_scrapy_spider/items.py
@@ -6,13 +6,29 @@
 import scrapy
 
 
-class EstItem(scrapy.Item):
+# class EstItem(scrapy.Item):
 
-    # rnum = scrapy.Field()
+#     # rnum = scrapy.Field()
+#     name = scrapy.Field()
+#     productCode = scrapy.Field()
+#     stock = scrapy.Field()
+#     regularPrice = scrapy.Field()
+#     salePrice = scrapy.Field()
+
+class EstItem(scrapy.Item):
+    # define the fields for your item here like:
+    ID = scrapy.Field()
     name = scrapy.Field()
+    category = scrapy.Field()
     productCode = scrapy.Field()
     stock = scrapy.Field()
+    tags = scrapy.Field()
+    availability = scrapy.Field()
     regularPrice = scrapy.Field()
     salePrice = scrapy.Field()
+    imageUrl = scrapy.Field()
+    productDesp = scrapy.Field()
+    ourRPrice = scrapy.Field()
+    ourSPrice =  scrapy.Field()
 
     
